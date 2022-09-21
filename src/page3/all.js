740939e1c392ef3feca3e52b181b1207b961c10d
Марина Максимова <m.maksimova@m-maksimova.local>
2022-09-21 18:23:27 +0300

    слайдер + страница фильма + страница всех фильмов

---
+++ src/page3/all.js
@@ -0,0 +1,25 @@
+import Card from "./card.js";
+
+const divFilms = document.querySelector('.all-films__block');
+
+const title = localStorage.getItem("title");
+
+const h3 = document.querySelector('.all-films__title');
+h3.textContent = title;
+
+let allCard = [];
+
+try {
+    allCard = JSON.parse(localStorage.getItem(title));
+
+    for (const item of allCard) {
+        let card = new Card(item);
+            card.render(divFilms);
+}
+} catch (error) {
+    console.log(error);
+    let errorMsg = document.createElement('p');
+    errorMsg.textContent = 'Sorry. Server is not avaliable.';
+    errorMsg.classList.add('error');
+    divFilms.append(errorMsg);
+}
---
+++ src/page3/card.js
@@ -0,0 +1,53 @@
+class Card {
+    constructor(obj) {
+        this.image = obj.image;
+        this.year = obj.year;
+        this.title = obj.title;
+        this.id = obj.id;
+    }
+
+    render(node) {
+        const fragment = new DocumentFragment();
+
+        const link = document.createElement('a');
+        link.href = "film.html";
+        link.classList.add('slider-arrow__card-link');
+        link.id = this.id;
+        link.addEventListener("click", function (e) {
+            let active = e.currentTarget;
+            let id = active.id;
+            localStorage.setItem("idFilm", id);
+        });
+
+        const card = document.createElement('div');
+        card.classList.add('slider-arrow__card');
+
+        const img = document.createElement('img');
+        img.alt = "poster";
+        img.src = this.image;
+        img.classList.add('slider-arrow__card-img');
+
+        const text = document.createElement('div');
+        text.classList.add('slider-arrow__card-text');
+
+        const time = document.createElement('p');
+        time.classList.add('slider-arrow__card-text__p');
+        time.textContent = this.year;
+
+        const titleElem = document.createElement('p');
+        titleElem.classList.add('slider-arrow__card-text__title');
+        titleElem.textContent = this.title;
+
+        fragment.append(link);
+        link.append(card);
+        card.append(img);
+        card.append(text);
+        text.append(titleElem);
+        text.append(time);
+
+        node.append(fragment);
+    }
+
+}
+
+export default Card;
